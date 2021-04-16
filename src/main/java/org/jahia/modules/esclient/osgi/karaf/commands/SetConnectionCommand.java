fb7ac309ac7e2b48093b9365ff998d11b4f67283
dsaulnier <dsaulnier@jahia.com>
2021-04-16 12:31:51 +0200

    Added the possibility to list the available connections

--- src/main/java/org/jahia/modules/esclient/osgi/karaf/commands/SetConnectionCommand.java
+++ src/main/java/org/jahia/modules/esclient/osgi/karaf/commands/SetConnectionCommand.java
@@ -1,5 +1,6 @@
 package org.jahia.modules.esclient.osgi.karaf.commands;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.karaf.shell.api.action.Action;
 import org.apache.karaf.shell.api.action.Argument;
 import org.apache.karaf.shell.api.action.Command;
@@ -7,12 +8,16 @@ import org.apache.karaf.shell.api.action.Completion;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.apache.karaf.shell.api.console.Session;
-import org.jahia.modules.esclient.osgi.karaf.Constants;
+import org.apache.karaf.shell.support.table.Col;
+import org.apache.karaf.shell.support.table.ShellTable;
 import org.jahia.modules.esclient.osgi.karaf.completers.ConnectionIDCompleter;
 import org.jahia.modules.esclient.services.ESService;
+import org.jahia.modules.esclient.services.ESServiceException;
 import org.jahia.osgi.BundleUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.jahia.modules.esclient.osgi.karaf.Constants.CONNECTION_ID;
 
 @Command(scope = "es", name = "connection", description = "Selects the connection to use")
 @Service
@@ -23,16 +28,44 @@ public class SetConnectionCommand implements Action {
     @Reference
     Session session;
 
-    @Argument(description = "Connection ID", required = true)
+    @Argument(description = "Connection ID. If not specified, the available connections are listed", required = false)
     @Completion(ConnectionIDCompleter.class)
     private String connection;
 
     @Override
     public Object execute() throws Exception {
-        session.put(Constants.CONNECTION_ID, connection);
-        final ESService esService = BundleUtils.getOsgiService(ESService.class, null);
-        esService.loadClient(connection);
-        System.out.println("Client loaded for the connection " + connection);
+        if (StringUtils.isBlank(connection)) {
+            printAvailableConnections();
+        } else {
+            setConnection(connection);
+        }
+
         return null;
+    }
+
+    private void setConnection(String id) {
+        final ESService esService = BundleUtils.getOsgiService(ESService.class, null);
+        try {
+            esService.loadClient(connection);
+            session.put(CONNECTION_ID, connection);
+            System.out.printf("Client loaded for the connection %s%n", connection);
+        } catch (ESServiceException e) {
+            logger.error("", e);
+            System.out.printf("Impossible to load the client for the connection %s : %s%n", connection, e.getMessage());
+        }
+    }
+
+    private void printAvailableConnections() {
+        final ESService esService = BundleUtils.getOsgiService(ESService.class, null);
+
+        final ShellTable table = new ShellTable();
+        table.column(new Col("Connection"));
+        table.column(new Col("Selected"));
+
+        for (String c : esService.listConnections()) {
+            table.addRow().addContent(c, StringUtils.equals(c, (String) session.get(CONNECTION_ID)) ? true : null);
+        }
+
+        table.print(System.out, true);
     }
 }
