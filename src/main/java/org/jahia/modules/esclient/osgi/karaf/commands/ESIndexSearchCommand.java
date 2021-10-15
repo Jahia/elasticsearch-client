87fc40c90974f8dcefdd47942e6b49644654b573
dsaulnier <dsaulnier@jahia.com>
2021-10-15 15:25:34 +0200

    Limiting the display to 20 entries

--- src/main/java/org/jahia/modules/esclient/osgi/karaf/commands/ESIndexSearchCommand.java
+++ src/main/java/org/jahia/modules/esclient/osgi/karaf/commands/ESIndexSearchCommand.java
@@ -1,21 +1,18 @@
 package org.jahia.modules.esclient.osgi.karaf.commands;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
 import org.apache.karaf.shell.api.action.Argument;
 import org.apache.karaf.shell.api.action.Command;
 import org.apache.karaf.shell.api.action.Completion;
 import org.apache.karaf.shell.api.action.Option;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.apache.karaf.shell.support.table.Col;
-import org.apache.karaf.shell.support.table.Row;
 import org.apache.karaf.shell.support.table.ShellTable;
 import org.jahia.modules.esclient.osgi.karaf.completers.IndicesCompleter;
 import org.jahia.modules.esclient.services.ESService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -42,25 +39,35 @@ public class ESIndexSearchCommand extends AbstractESCommand {
             return null;
         }
 
-        if (filters.size()%2 != 0) {
+        if (filters.size() % 2 != 0) {
             System.out.println("Invalid filters. The number of values must match the number of fields");
             return null;
         }
 
-        final Map<String,String> queryFilters = new HashMap<>();
+        final Map<String, String> queryFilters = new HashMap<>();
         int i = 0;
-        while (i<filters.size()) queryFilters.put(filters.get(i++), filters.get(i++));
+        while (i < filters.size()) queryFilters.put(filters.get(i++), filters.get(i++));
 
         final ESService esService = getESService();
         final List<String> hits = esService.getHits(index, queryFilters);
 
+        final int size = hits.size();
+        if (size == 0) {
+            System.out.println("Nothing matched");
+            return null;
+        }
+
         final ShellTable table = new ShellTable();
         table.column(new Col("Hit"));
-        for (String hit : hits) {
-            table.addRow().addContent(hit);
+        for (int j = 0; j < 20 && j < size; j++) {
+            table.addRow().addContent(hits.get(j));
         }
+
         table.noHeaders();
         table.print(System.out, true);
+        if (size > 20) {
+            System.out.println(String.format("%d entries found in the index, displaying only the first 20", size));
+        }
 
         return null;
     }
